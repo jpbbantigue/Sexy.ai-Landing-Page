84e7c39adba792aaf68c886961b6ac19fd7667e8
jpbbantigue <jpbbantigue@ymail.com>
2024-03-27 19:42:24 +0800

    Updates to pricing section
    - reimplement pricing frequency toggle
    - payment option availability
    - minor text changes

--- assets/scripts.js
+++ assets/scripts.js
@@ -23,6 +23,33 @@ $(document).ready(function(){
     nsfwModal.toggle();
   });
 
+  $("#pricingToggle").click(function(e){
+
+    if ( $("#pricingToggle").prop('checked') == false) {
+      $("#pricingToggle + label").text("Monthly");
+      $("#pricingToggle").prop('checked',false);
+      $("#pricingOption1 + label .display-6").text('$14.99');
+      $("#pricingOption1 + label .cc-btn").attr('href','https://api.sexy.ai/initializeEpochSubscription?sessionID=b8916431-fcf6-42b0-bfc1-e06c4d187096&productID=6vcQ421AmlbN&redirectURL=https%3A%2F%2Fsexy.ai%2Fgenerate');
+      
+      $("#pricingOption2 + label .display-6").text('$29.99');
+      $("#pricingOption1 + label .cc-btn").attr('href','https://api.sexy.ai/initializeEpochSubscription?sessionID=b8916431-fcf6-42b0-bfc1-e06c4d187096&productID=lPQ7Okzw2ead&redirectURL=https%3A%2F%2Fsexy.ai%2Fgenerate');
+
+      $(".freq").text('mo');
+      $(".text-decoration-line-through, .crypto-btn, .annual").hide();
+    } else {
+      $("#pricingToggle + label").text("Annual");
+      $("#pricingToggle").prop('checked',true);
+      $("#pricingOption1 + label .display-6").text('$7.50 ');
+      $("#pricingOption1 + label .cc-btn").attr('href','https://api.sexy.ai/initializeEpochSubscription?sessionID=b8916431-fcf6-42b0-bfc1-e06c4d187096&productID=Hbu5RCfvDYFu&redirectURL=https%3A%2F%2Fsexy.ai%2Fgenerate');
+      
+      $("#pricingOption2 + label .display-6").text('$15.00 ');
+      $("#pricingOption1 + label .cc-btn").attr('href','https://api.sexy.ai/initializeEpochSubscription?sessionID=b8916431-fcf6-42b0-bfc1-e06c4d187096&productID=XikPabvBcGjH&redirectURL=https%3A%2F%2Fsexy.ai%2Fgenerate');
+
+      $(".freq").text('mo');
+      $(".text-decoration-line-through, .crypto-btn, .annual").show();
+    }
+  });
+
   $(".form-check-input").on('change',function(){
     if (this.id == 'pricingOption1') {
       $(".annual-cost").text("90.00");
